acb43107f6a78eb7e75abe8c8afa9cb27bb49a92
ZedZed34 <haun070@aucklanduni.ac.nz>
2025-08-14 03:50:54 +0800

    Fix CSS import case sensitivity for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { UserProvider } from './context/UserContext';
 import Layout from './components/Layout';
 import Overview from './pages/Overview';
 import WorkoutPlan from './pages/WorkoutPlan';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
